6be73e355763ce5bc180828adc94466d7d22f098
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-29 22:01:40 -0600

    Add generic random number generator within a function.

---
+++ RollOfTheDice/RollOfTheDice.vb
@@ -0,0 +1,24 @@
+﻿'Elliot Heiner
+'RCET 0265
+'Fall 2021
+'Roll of the Dice
+'https://github.com/heinelli/RollOfTheDice.git
+
+Module RollOfTheDice
+
+    Sub Main()
+        For i = 0 To 50
+            Console.WriteLine(RandomNumber())
+        Next
+        Console.Read()
+
+    End Sub
+
+    Function RandomNumber()
+        Dim rndNumber As Integer
+        Randomize()
+        rndNumber = Int((11 * Rnd()) + 1)
+        Return rndNumber
+    End Function
+
+End Module
