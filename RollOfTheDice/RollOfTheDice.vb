ca2c17afb45442dc013bdebeec41d2bcbdb08d13
Elliot Heiner <elliotheiner1997@gmail.com>
2021-10-03 19:31:56 -0600

    Add comments.

--- RollOfTheDice/RollOfTheDice.vb
+++ RollOfTheDice/RollOfTheDice.vb
@@ -3,38 +3,38 @@
 'Fall 2021
 'Roll of the Dice
 'https://github.com/heinelli/RollOfTheDice.git
-
 Module RollOfTheDice
-
     Sub Main()
         Dim diceNumbers(10) As Integer
 
-
+        'Total the number of times that each combination is rolled.
         For i = 0 To 1000
             diceNumbers(RandomNumber()) += 1
         Next
 
+        'Create a header with the proper amount of columns
         For i = LBound(diceNumbers) To UBound(diceNumbers)
             Console.Write(CStr(i + 2).PadLeft(4) & " |")
         Next
 
+        'Create a new line and draw line between top and bottom row.
         Console.WriteLine()
         Console.WriteLine(StrDup(66, "-"))
 
+        'Display the previously totaled combination count
         For i = LBound(diceNumbers) To UBound(diceNumbers)
             Console.Write(CStr(diceNumbers(i)).PadLeft(4) & " |")
         Next
         Console.Read()
-
     End Sub
-
     Function RandomNumber()
         Dim rndNumber1 As Integer
         Dim rndNumber2 As Integer
+
+        'Create two random values from 1-6 and add them together.
         Randomize()
         rndNumber1 = Int((6 * Rnd()) + 1)
         rndNumber2 = Int((6 * Rnd()) + 1)
         Return rndNumber1 + rndNumber2 - 2
     End Function
-
 End Module
